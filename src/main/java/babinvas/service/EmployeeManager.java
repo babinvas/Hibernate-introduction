505838cc1e6d81e8a0416550a8fd3cb46d99e1ae
babinvas <babinvas@gmail.com>
2021-02-25 19:00:38 +0300

    The CRUD class was implemented to handle the employee class

--- src/main/java/babinvas/service/EmployeeManager.java
+++ src/main/java/babinvas/service/EmployeeManager.java
@@ -1,32 +1,143 @@
 package babinvas.service;
 
+import babinvas.repository.Employee;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class EmployeeManager {
-	private static SessionFactory factory;
+	private final SessionFactory factory;
 
-	public static void main(String[] args) {
+	public EmployeeManager(SessionFactory factory) {
+		this.factory = factory;
 	}
 
 	// Reads all the employees
 	public void listEmployees() {
+		Session session = factory.openSession();
+		Transaction transaction = null;
+
+		try {
+			transaction = session.beginTransaction();
+
+			List employees = session.createQuery("from Employee").list();
+
+			for (int i = employees.size() - 1; i >= 0; i--) {
+				Employee employee = (Employee) employees.get(i);
+
+				System.out.println("First Name: " + employee.getFirstName() +
+						"\t" + "Last Name: " + employee.getLastName() +
+						"\t" + "Salary: " + employee.getSalary());
+			}
+		} catch(HibernateException e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 	}
 
-	// Create an employee in the database
+	// Creates an employee in the database
 	public Integer addEmployee(String firstName, String lastName, int salary) {
-		return 0;
+		Session session = factory.openSession();
+		Transaction transaction = null;
+		Integer employeeId = null;
+
+		try{
+			transaction = session.beginTransaction();
+
+			Employee employee = new Employee(firstName, lastName, salary);
+			employeeId = (Integer) session.save(employee);
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return employeeId;
 	}
 
 	// Reads salary for an employee
-	public void readEmployee(Integer EmployeeID) {
+	public Employee readEmployee(Integer EmployeeId) {
+		Session session = factory.openSession();
+		Transaction transaction = null;
+		Employee employee = null;
+
+		try {
+			transaction = session.beginTransaction();
+
+			employee = session.get(Employee.class, EmployeeId);
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return employee;
 	}
 
 	// Updates salary for an employee
-	public void updateEmployee(Integer EmployeeID, int salary) {
+	public void updateEmployee(Integer EmployeeId, int salary) {
+		Session session = factory.openSession();
+		Transaction transaction = null;
+
+		try {
+			transaction = session.beginTransaction();
+
+			Employee employee = session.get(Employee.class, EmployeeId);
+			employee.setSalary(salary);
+			session.update(employee);
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 	}
 
 	// Deletes an employee from the records
-	public void deleteEmployee(Integer EmployeeID) {
+	public void deleteEmployee(Integer EmployeeId) {
+		Session session = factory.openSession();
+		Transaction transaction = null;
 
+		try {
+			transaction = session.beginTransaction();
+
+			Employee employee = session.get(Employee.class, EmployeeId);
+			session.delete(employee);
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 	}
 }
